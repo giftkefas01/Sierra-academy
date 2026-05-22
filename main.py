45d718bcb930c704c19d60329a674c74b0898d88
giftkefas01 <giftkefas2@gmail.com>
2026-05-22 14:03:18 -0700

    messages goes here

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from operator import truediv, add
+
+name = 'gift' #string variable
+age = 17
+height = 6.1
+is_online = "TRUE"
+#camelCase:  (e.g., firstName, calculateTotalAmount).
+#PascalCase: (e.g., FirstName, CustomerRecord).
+#snake_case:  (e.g., first_name, user_account_id).
+#kebab-case:  (e.g., first-name, button-style).
+#(MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global
