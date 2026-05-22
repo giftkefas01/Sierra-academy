af42b6127982aa9165dbe061f3374cc40e270066
giftkefas01 <giftkefas2@gmail.com>
2026-05-22 14:15:49 -0700

    messages goes here

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from operator import truediv, add
+
 
 name = 'gift' #string variable
 age = 17
 height = 6.1
-is_online = "TRUE"
+is_online = True
 #camelCase:  (e.g., firstName, calculateTotalAmount).
 #PascalCase: (e.g., FirstName, CustomerRecord).
 #snake_case:  (e.g., first_name, user_account_id).
