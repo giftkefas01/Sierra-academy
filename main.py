3ad043c9b32b0d70f935b3f1bf647bce94707aed
giftkefas01 <giftkefas2@gmail.com>
2026-05-22 14:22:18 -0700

    messagees goes here

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 
 name = 'gift' #string variable
-age = 17
-height = 6.1
-is_online = True
-#camelCase:  (e.g., firstName, calculateTotalAmount).
-#PascalCase: (e.g., FirstName, CustomerRecord).
-#snake_case:  (e.g., first_name, user_account_id).
-#kebab-case:  (e.g., first-name, button-style).
-#(MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global
+age = 17 # this is an integer variable
+height = 6.1 # this is a decimal variable
+is_online = True # booylean variable
+# camelCase:  (e.g., firstName, calculateTotalAmount).
+# PascalCase: (e.g., FirstName, CustomerRecord).
+# snake_case:  (e.g., first_name, user_account_id).
+# kebab-case:  (e.g., first-name, button-style).
+# (MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global
